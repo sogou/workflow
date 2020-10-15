e884a6902ccbd626c486920454412b1ee5b6f850
xiehan <52160700+Barenboim@users.noreply.github.com>
2020-10-15 16:40:01 +0800

    Add WFGraphTask to windows branch. (#103)
    * Add WFGraphTask
    
    Co-authored-by: XieHan <XieHan@sogou-inc.com>

---
+++ src/factory/WFGraphTask.cc
@@ -0,0 +1,95 @@
+/*
+  Copyright (c) 2020 Sogou, Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  Author: Xie Han (xiehan@sogou-inc.com)
+*/
+
+#include <vector>
+#include "Workflow.h"
+#include "WFGraphTask.h"
+
+SubTask *WFGraphNode::done()
+{
+	SeriesWork *series = series_of(this);
+
+	if (!this->user_data)
+	{
+		this->value = 1;
+		this->user_data = (void *)1;
+	}
+	else
+		delete this;
+
+	return series->pop();
+}
+
+WFGraphNode::~WFGraphNode()
+{
+	if (this->user_data)
+	{
+		for (WFGraphNode *node : this->successors)
+			node->WFCounterTask::count();
+	}
+}
+
+WFGraphNode& WFGraphTask::create_graph_node(SubTask *task)
+{
+	WFGraphNode *node = new WFGraphNode;
+	SeriesWork *series = Workflow::create_series_work(node, node, nullptr);
+
+	series->push_back(task);
+	this->parallel->add_series(series);
+	return *node;
+}
+
+SubTask *WFGraphTask::done()
+{
+	SeriesWork *series = series_of(this);
+	ParallelWork *parallel = this->parallel;
+
+	if (parallel)
+	{
+		this->parallel = NULL;
+		series->push_front(this);
+		series->push_front(parallel);
+	}
+	else
+	{
+		if (this->callback)
+			this->callback(this);
+
+		delete this;
+	}
+
+	return series->pop();
+}
+
+WFGraphTask::~WFGraphTask()
+{
+	SeriesWork *series;
+	size_t i;
+
+	if (this->parallel)
+	{
+		for (i = 0; i < this->parallel->size(); i++)
+		{
+			series = const_cast<SeriesWork *>(this->parallel->series_at(i));
+			series->set_last_task(new WFGenericTask);
+		}
+
+		this->parallel->dismiss();
+	}
+}
+

6e5c7f6abe6948855919cd906896a28a99e6a9d5
XieHan <XieHan@sogou-inc.com>
2021-01-28 14:54:08 +0800

    update message's move constructors

--- src/protocol/HttpMessage.h
+++ src/protocol/HttpMessage.h
@@ -275,12 +275,8 @@ public:
 
 	/* for std::move() */
 public:
-	HttpRequest(HttpRequest&& req) : HttpMessage(std::move(req)) { }
-	HttpRequest& operator = (HttpRequest&& req)
-	{
-		*(HttpMessage *)this = std::move(req);
-		return *this;
-	}
+	HttpRequest(HttpRequest&& req) = default;
+	HttpRequest& operator = (HttpRequest&& req) = default;
 };
 
 class HttpResponse : public HttpMessage
@@ -358,12 +354,8 @@ public:
 
 	/* for std::move() */
 public:
-	HttpResponse(HttpResponse&& resp) : HttpMessage(std::move(resp)) { }
-	HttpResponse& operator = (HttpResponse&& resp)
-	{
-		*(HttpMessage *)this = std::move(resp);
-		return *this;
-	}
+	HttpResponse(HttpResponse&& resp) = default;
+	HttpResponse& operator = (HttpResponse&& resp) = default;
 };
 
 }

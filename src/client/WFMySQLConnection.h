3f273ad116d16797d441cd91e4845dc127814fb4
liyingxin <liyingxin1412@gmail.com>
2021-04-12 20:18:57 +0800

    add define guard for WFMySQLConnection.h (#335)

--- src/client/WFMySQLConnection.h
+++ src/client/WFMySQLConnection.h
@@ -16,6 +16,9 @@
   Author: Xie Han (xiehan@sogou-inc.com)
 */
 
+#ifndef _WFMYSQLCONNECTION_H_
+#define _WFMYSQLCONNECTION_H_
+
 #include <string>
 #include <utility>
 #include <functional>
@@ -72,4 +75,6 @@ WFMySQLConnection::create_disconnect_task(mysql_callback_t callback)
 	task->set_keep_alive(0);
 	return task;
 }
+
+#endif
 

8bb2bb0e6ad0cc68622adf22274372fdd1e57ffe
dapengwang <dapengwang@tencent.com>
2022-03-17 09:11:55 +0800

    fix copy right

--- src/protocol/ConsulDataTypes.h
+++ src/protocol/ConsulDataTypes.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2020 Sogou, Inc.
+  Copyright (c) 2022 Sogou, Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.

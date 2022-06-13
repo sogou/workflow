15f1524067bea5c010d1a31550b8befe5bae01ba
XieHan <XieHan@sogou-inc.com>
2022-06-13 20:48:45 +0800

    Call virtual 'handle()' if dispatch failed.

--- src/kernel/CommRequest.cc
+++ src/kernel/CommRequest.cc
@@ -26,27 +26,13 @@ void CommRequest::handle(int state, int error)
 	this->error = error;
 	if (error != ETIMEDOUT)
 		this->timeout_reason = TOR_NOT_TIMEOUT;
+	else if (!this->target)
+		this->timeout_reason = TOR_WAIT_TIMEOUT;
 	else if (!this->get_message_out())
 		this->timeout_reason = TOR_CONNECT_TIMEOUT;
 	else
 		this->timeout_reason = TOR_TRANSMIT_TIMEOUT;
 
 	this->subtask_done();
-}
-
-void CommRequest::dispatch()
-{
-	if (this->scheduler->request(this, this->object, this->wait_timeout,
-								 &this->target) < 0)
-	{
-		this->state = CS_STATE_ERROR;
-		this->error = errno;
-		if (errno != ETIMEDOUT)
-			this->timeout_reason = TOR_NOT_TIMEOUT;
-		else
-			this->timeout_reason = TOR_WAIT_TIMEOUT;
-
-		this->subtask_done();
-	}
 }
 

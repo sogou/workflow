787669af0cd1c2f304f913f14f953da57cb0e893
Chanchan <www.769440972@qq.com>
2023-01-04 22:08:28 +0800

    fix : upstream config typo

--- src/nameservice/xmake.lua
+++ src/nameservice/xmake.lua
@@ -1,7 +1,7 @@
 target("nameservice")
     add_files("*.cc")
     set_kind("object")
-    if not has_config("upstrem") then
+    if not has_config("upstream") then
         remove_files("WFServiceGovernance.cc", "UpstreamPolicies.cc")
     end
 

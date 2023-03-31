3c74637bd0f43bc8a0c3551c2331bebd7d509d67
kedixa <1204837541@qq.com>
2023-03-31 16:43:05 +0800

    kafkabugfix: OffsetCommit resp version >=3 has throttle_time

--- src/protocol/KafkaMessage.cc
+++ src/protocol/KafkaMessage.cc
@@ -3625,7 +3625,7 @@ int KafkaResponse::parse_offsetcommit(void **buf, size_t *size)
 	int32_t partition_cnt;
 	int32_t partition;
 
-	if (this->api_version >= 1)
+	if (this->api_version >= 3)
 		CHECK_RET(parse_i32(buf, size, &throttle_time));
 
 	CHECK_RET(parse_i32(buf, size, &topic_cnt));

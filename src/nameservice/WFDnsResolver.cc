d894da0a589bf05b51ee4259010432ad5ae79def
kedixa <1204837541@qq.com>
2023-05-17 17:22:44 +0800

    move DnsRoutine to WFDnsResolver

--- src/nameservice/WFDnsResolver.cc
+++ src/nameservice/WFDnsResolver.cc
@@ -41,6 +41,119 @@
 #define HOSTS_LINEBUF_INIT_SIZE	128
 #define PORT_STR_MAX			5
 
+class DnsInput
+{
+public:
+	DnsInput() :
+		port_(0),
+		numeric_host_(false)
+	{}
+
+	DnsInput(const std::string& host, unsigned short port,
+			 bool numeric_host) :
+		host_(host),
+		port_(port),
+		numeric_host_(numeric_host)
+	{}
+
+	void reset(const std::string& host, unsigned short port)
+	{
+		host_.assign(host);
+		port_ = port;
+		numeric_host_ = false;
+	}
+
+	void reset(const std::string& host, unsigned short port,
+			   bool numeric_host)
+	{
+		host_.assign(host);
+		port_ = port;
+		numeric_host_ = numeric_host;
+	}
+
+	const std::string& get_host() const { return host_; }
+	unsigned short get_port() const { return port_; }
+	bool is_numeric_host() const { return numeric_host_; }
+
+protected:
+	std::string host_;
+	unsigned short port_;
+	bool numeric_host_;
+
+	friend class DnsRoutine;
+};
+
+class DnsOutput
+{
+public:
+	DnsOutput():
+		error_(0),
+		addrinfo_(NULL)
+	{}
+
+	~DnsOutput()
+	{
+		if (addrinfo_)
+			freeaddrinfo(addrinfo_);
+	}
+
+	int get_error() const { return error_; }
+	const struct addrinfo *get_addrinfo() const { return addrinfo_; }
+
+	//if DONOT want DnsOutput release addrinfo, use move_addrinfo in callback
+	struct addrinfo *move_addrinfo()
+	{
+		struct addrinfo *p = addrinfo_;
+		addrinfo_ = NULL;
+		return p;
+	}
+
+protected:
+	int error_;
+	struct addrinfo *addrinfo_;
+
+	friend class DnsRoutine;
+};
+
+class DnsRoutine
+{
+public:
+	static void run(const DnsInput *in, DnsOutput *out);
+	static void create(DnsOutput *out, int error, struct addrinfo *ai)
+	{
+		if (out->addrinfo_)
+			freeaddrinfo(out->addrinfo_);
+
+		out->error_ = error;
+		out->addrinfo_ = ai;
+	}
+};
+
+void DnsRoutine::run(const DnsInput *in, DnsOutput *out)
+{
+	if (!in->host_.empty() && in->host_[0] == '/')
+		return;
+
+	struct addrinfo hints = {
+#ifdef AI_ADDRCONFIG
+		/*.ai_flags    = */AI_ADDRCONFIG,
+#endif
+		/*.ai_family   = */AF_UNSPEC,
+		/*.ai_socktype = */SOCK_STREAM
+	};
+	char port_str[PORT_STR_MAX + 1];
+
+	hints.ai_flags |= AI_NUMERICSERV;
+	if (in->is_numeric_host())
+		hints.ai_flags |= AI_NUMERICHOST;
+
+	snprintf(port_str, PORT_STR_MAX + 1, "%u", in->port_);
+	out->error_ = getaddrinfo(in->host_.c_str(),
+							  port_str,
+							  &hints,
+							  &out->addrinfo_);
+}
+
 // Dns Thread task. For internal usage only.
 using ThreadDnsTask = WFThreadTask<DnsInput, DnsOutput>;
 using thread_dns_callback_t = std::function<void (ThreadDnsTask *)>;

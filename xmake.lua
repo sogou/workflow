981ebc05a0bfce6f7553743d94079fda8ca72fb0
Xie Han <63350856@qq.com>
2024-04-23 22:17:27 +0800

    0.11.3 -> 0.11.4

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,5 @@
 set_project("workflow")
-set_version("0.11.3")
+set_version("0.11.4")
 
 option("workflow_inc",  {description = "workflow inc", default = "$(projectdir)/_include"})
 option("workflow_lib",  {description = "workflow lib", default = "$(projectdir)/_lib"})

3cc0f16eea494862a50452dfef24bacaa4b037f8
Xie Han <63350856@qq.com>
2025-05-08 15:18:47 +0800

    Remove LICENSEs in tutorials.

--- tutorial/tutorial-10-user_defined_protocol/message.h
+++ tutorial/tutorial-10-user_defined_protocol/message.h
@@ -1,21 +1,3 @@
-/*
-  Copyright (c) 2020 Sogou, Inc.
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-	  http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-  Author: Xie Han (xiehan@sogou-inc.com;63350856@qq.com)
-*/
-
 #ifndef _TUTORIALMESSAGE_H_
 #define _TUTORIALMESSAGE_H_
 
